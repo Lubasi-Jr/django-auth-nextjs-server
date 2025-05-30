10de8e21b82a337e39fb9ea6aa703f67d85c28cb
Lubasi Milupi <lubasimilupi@gmail.com>
2025-05-30 19:36:39 +0200

    Life is coherent

--- api/api/settings.py
+++ api/api/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1=2udbcps0z%o_7$0eaul2l-bvjo&sp#5@2#r_elfee^tf^bh='
+SECRET_KEY = os.getenv('SECRECT_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
