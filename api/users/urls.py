b95715d1be548ee5d6f871c592d9a6f13c03cf81
Lubasi Milupi <lubasimilupi@gmail.com>
2025-05-31 17:20:25 +0200

    URL configuration

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import UserInfoView, UserRegistrationView
 
+# URL's are going to start with api/users
+
 urlpatterns = [
     path('user-info/',UserInfoView.as_view(), name='user-info'),
     path('register/', UserRegistrationView.as_view(), name='register-user')
