4530654f321ff22afac86e8177566418b86febe2
Lubasi Milupi <lubasimilupi@gmail.com>
2025-06-01 22:49:20 +0200

    Basic structure of the LoginView

--- api/users/views.py
+++ api/users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.generics import RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from .serializers import CustomUserSerializer, RegisterUserSerializer
+from .serializers import CustomUserSerializer, RegisterUserSerializer, LoginUserSerializer
 
-# Create your views here.
+# Create your views here
 class UserInfoView(RetrieveUpdateAPIView):
     permission_classes = (IsAuthenticated)
     serializer_class = CustomUserSerializer
@@ -13,3 +14,10 @@ class UserInfoView(RetrieveUpdateAPIView):
 
 class UserRegistrationView(CreateAPIView):
     serializer_class = RegisterUserSerializer
+
+class LoginView(APIView):
+    def post(self,request):
+        serializer = LoginUserSerializer(data= request.data)
+
+        # Generate JWT token
+
